cea1805f10429b89f953ade7f1e056a7e2e13d10
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-01-04 06:16:52 +0300

    F105/7 support

---
+++ rules_stm32f105_107.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2014 Zubax, zubax.com
+# Distributed under the MIT License, available in the file LICENSE.
+# Author: Pavel Kirienko <pavel.kirienko@zubax.com>
+#
+
+ZUBAX_CHIBIOS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+CPPSRC += $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/sys/sys_stm32.cpp                  \
+          $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/watchdog/watchdog_stm32.cpp        \
+          $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/config/config_storage_stm32.cpp    \
+
+CHIBIOS := $(ZUBAX_CHIBIOS_DIR)/chibios
+include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32f1xx.mk
+include $(CHIBIOS)/os/hal/ports/STM32/STM32F1xx/platform_f105_f107.mk
+include $(CHIBIOS)/os/rt/ports/ARMCMx/compilers/GCC/mk/port_v7m.mk
+
+LDSCRIPT ?= $(PORTLD)/STM32F107xC.ld
+
+MCU = cortex-m3
+
+include $(ZUBAX_CHIBIOS_DIR)/_rules_armcm.mk
