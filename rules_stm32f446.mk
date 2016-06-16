8ea7fa8eb1142bc9faea8e6e272a3b3589e68989
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-06-16 19:59:49 +0300

    F446 support

---
+++ rules_stm32f446.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2016 Zubax, zubax.com
+# Distributed under the MIT License, available in the file LICENSE.
+# Author: Pavel Kirienko <pavel.kirienko@zubax.com>
+#
+
+ZUBAX_CHIBIOS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+CPPSRC += $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/platform/stm32/sys_stm32.cpp               \
+          $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/platform/stm32/watchdog_stm32.cpp          \
+          $(ZUBAX_CHIBIOS_DIR)/zubax_chibios/platform/stm32/config_storage_stm32.cpp    \
+
+CHIBIOS := $(ZUBAX_CHIBIOS_DIR)/chibios
+include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32f4xx.mk
+include $(CHIBIOS)/os/hal/ports/STM32/STM32F4xx/platform.mk
+include $(CHIBIOS)/os/rt/ports/ARMCMx/compilers/GCC/mk/port_v7m.mk
+
+ifeq ($(LDSCRIPT),)
+    $(error Default LDSCRIPT is not available for this platform; the application must provide a custom linker script.)
+endif
+
+USE_FPU = hard
+MCU = cortex-m4
+
+include $(ZUBAX_CHIBIOS_DIR)/_rules_armcm.mk
