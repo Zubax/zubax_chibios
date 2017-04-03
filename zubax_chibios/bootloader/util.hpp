5d4d54a4e47b2d57a2443118841e75500fa1cea7
Pavel Kirienko <pavel.kirienko@gmail.com>
2017-04-03 04:01:24 +0300

    Optimized CRC64WE

--- zubax_chibios/bootloader/util.hpp
+++ zubax_chibios/bootloader/util.hpp
@@ -26,6 +26,10 @@ static constexpr std::int16_t ErrAppStorageWriteFailure = 10003;
 
 /**
  * This is used to verify integrity of the application and other data.
+ * Note that firmware CRC verification is a very computationally intensive process that needs to be completed
+ * in a limited time interval, which should be minimized. Therefore, this class has been carefully manually
+ * optimized to achieve the optimal balance between speed and ROM footprint.
+ *
  * CRC-64-WE
  * Description: http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat-bits.64
  * Initial value: 0xFFFFFFFFFFFFFFFF
@@ -36,34 +40,29 @@ static constexpr std::int16_t ErrAppStorageWriteFailure = 10003;
  */
 class CRC64WE
 {
+    static constexpr std::uint64_t Poly = 0x42F0E1EBA9EA3693;
+    static constexpr std::uint64_t Mask = std::uint64_t(1) << 63;
+
     std::uint64_t crc_ = 0xFFFFFFFFFFFFFFFFULL;
 
 public:
-    void add(std::uint8_t byte)
-    {
-        static constexpr std::uint64_t Poly = 0x42F0E1EBA9EA3693;
-        static constexpr std::uint64_t Mask = std::uint64_t(1) << 63;
-
-        crc_ ^= std::uint64_t(byte) << 56;
-
-        // Manual unrolling here speeds up the image CRC verification loop by 20%, this is huge
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-        crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
-    }
-
     void add(const void* data, unsigned len)
     {
         auto bytes = static_cast<const std::uint8_t*>(data);
         assert(bytes != nullptr);
         while (len --> 0)
         {
-            add(*bytes++);
+            crc_ ^= std::uint64_t(*bytes++) << 56;
+
+            // Do not fold this into loop! The difference in performance can be drastic.
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
+            crc_ = (crc_ & Mask) ? (crc_ << 1) ^ Poly : crc_ << 1;
         }
     }
 
